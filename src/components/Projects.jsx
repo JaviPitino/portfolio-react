da4c55e4a882e4af6247bcf7c1eb15c9a728e8bb
Javier López <javierpitino@gmail.com>
2022-07-08 11:16:51 +0200

    fotos tres proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,59 @@
-import React from 'react'
-import { Card, Button } from 'react-bootstrap'
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 function Projects() {
   return (
     <section id="projects">
-        <div className="container-projects">
+      <div className="container-projects">
         <div className="line-about"></div>
         <h3>Proyectos</h3>
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="./imgs/road-to-hell.png" />
-            <Card.Body>
-                <Card.Title>Road To Hell</Card.Title>
-                <Card.Text>
-                Un pequeño juego de colisiones con estético ochentera realizado con javascript. Dosis de rock, cerveza, un poquito de irreverencia y mucho humor
-                </Card.Text>
-                <Button variant="primary">Go Hell!</Button>
-            </Card.Body>
-            </Card>
-        </div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="./imgs/road-to-hell.png" />
+          <Card.Body>
+            <Card.Title>Road To Hell</Card.Title>
+            <Card.Text>
+              Un pequeño juego de colisiones con estético ochentera realizado
+              con javascript. Dosis de rock, cerveza, un poquito de irreverencia
+              y mucho humor
+            </Card.Text>
+            <Button variant="primary">Go Hell!</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="container-projects">
+        <div className="line-about"></div>
+        <h3>Proyectos</h3>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="./imgs/wines-routes.png" />
+          <Card.Body>
+            <Card.Title>Road To Hell</Card.Title>
+            <Card.Text>
+              Un pequeño juego de colisiones con estético ochentera realizado
+              con javascript. Dosis de rock, cerveza, un poquito de irreverencia
+              y mucho humor
+            </Card.Text>
+            <Button variant="primary">Go Hell!</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="container-projects">
+        <div className="line-about"></div>
+        <h3>Proyectos</h3>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="./imgs/ecocajas.png" />
+          <Card.Body>
+            <Card.Title>Road To Hell</Card.Title>
+            <Card.Text>
+              Un pequeño juego de colisiones con estético ochentera realizado
+              con javascript. Dosis de rock, cerveza, un poquito de irreverencia
+              y mucho humor
+            </Card.Text>
+            <Button variant="primary">Go Hell!</Button>
+          </Card.Body>
+        </Card>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
